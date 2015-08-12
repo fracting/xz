ad80f0667b6ec12e4f76d8150832ea0c7199a471
Qian Hong <qhong@codeweavers.com>
2015-08-12 20:41:55 +0800

    Test new travis scripts.

---
+++ travis/build_msys2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash /usr/bin/msys2-shell
+
+set -x
+
+ls
+pwd
+REPO_PATH=`pwd`
+
+PACKAGES_ROOT=${HOME}/MSYS2-packages
+if ! test -d ${PACKAGES_ROOT}
+then
+    mkdir ${PACKAGES_ROOT}
+fi
+
+PACKAGE=xz
+PKGBUILD_DIR=${PACKAGE}-git
+if test -d ${PACKAGES_ROOT}/${PKGBUILD_DIR}
+then
+    rm -rf ${PACKAGES_ROOT}/${PKGBUILD_DIR}
+fi
+
+svn co https://github.com/fracting/MSYS2-packages/trunk/${PKGBUILD_DIR} ${PACKAGES_ROOT}/${PKGBUILD_DIR}
+
+# Working around wine bug which causes msys2 git fail to clone remote repo
+cd ${PACKAGES_ROOT}/${PKGBUILD_DIR}
+mkdir src
+pushd src
+git clone -l ${REPO_PATH} ${PACKAGE}
+cp -v ../* . || echo skip directory
+popd
+
+# Emulate prepare(), which won't be called with --noextract
+(export srcdir=$(realpath src); . PKGBUILD; prepare)
+
+makepkg -s -f --noconfirm --skippgpcheck --noextract ${NOCHECK}
